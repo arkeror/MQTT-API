f828314ba77da0bc01471da9367324a1e52df335
arkeror <37896129+arkeror@users.noreply.github.com>
2019-07-31 13:57:57 +0800

    Base

---
+++ MQTT API/Controllers/MQTTController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MQTT_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MQTTController : ControllerBase
+    {
+        [HttpGet]
+        public string OnlineCheck()
+        {
+            return "Online";
+        }
+
+        //Invoked by a Form JSON
+        [HttpPost]
+        public Payload MQTTPost([FromForm]Payload FormData)
+        {
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Payload));
+            MemoryStream msObj = new MemoryStream();
+            js.WriteObject(msObj, FormData);
+            msObj.Position = 0;
+            StreamReader sr = new StreamReader(msObj);
+            string json = sr.ReadToEnd();
+
+            MQTT.Publish(json);//MQTT Publish
+
+            return FormData;//Return Input Data
+        }
+    }
+    public class Payload
+    {
+        public string Variable_1 { get; set; }
+        public string Variable_2 { get; set; }
+        public string Variable_3 { get; set; }
+    }
+}
---
+++ MQTT API/MQTT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+
+namespace MQTT_API
+{
+    public class MQTT
+    {
+        private static MqttClient IotClient;
+
+        public static void Init()
+        {
+            string IotEndPoint = "*********-ats.iot.us-east-2.amazonaws.com"; //AWS Endpoint
+            X509Certificate CaCert = X509Certificate.CreateFromCertFile(@"C:\Location\root-CA.crt"); //Directory of AWS Root Certificate
+            X509Certificate ClientCert = new X509Certificate2(@"C:\Location\Cert.pfx", "PasswordOfPFXFile"); //Directory of Device Certificate in pfx format and password for the pfx file
+
+            string ClientId = Guid.NewGuid().ToString();
+            IotClient = new MqttClient(IotEndPoint, 8883, true, CaCert, ClientCert, MqttSslProtocols.TLSv1_2);
+            IotClient.Connect(ClientId);
+            //while (!IotClient.IsConnected) { }
+        }
+
+        //Default Message when no message is specified 
+        public static void Publish()
+        {
+            string Topic = "***************"; //MQTT Topic
+            string Message = "****"; //Message
+            IotClient.Publish(Topic, Encoding.UTF8.GetBytes(Message));
+        }
+
+        public static void Publish(string Message)
+        {
+            string Topic = "***************"; //MQTT Topic
+            IotClient.Publish(Topic, Encoding.UTF8.GetBytes(Message));
+        }
+    }
+}
